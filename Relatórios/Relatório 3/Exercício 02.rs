c5862109d9f793b107f80ee5ee0ef683508cbb94
Lavnia005 <laviniaandrade005@gmail.com>
2026-08-31 10:52:52 -0300

    Create Exercício 02.rs

---
+++ Relatórios/Relatório 3/Exercício 02.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+fn acertou_o_alvo(palpite: i32, numero_secreto: i32) -> bool {
+    (palpite - numero_secreto).abs() <= 5
+}
+
+fn main() {
+    let numero_secreto: i32 = 42;
+
+    loop {
+        let mut entrada = String::new();
+        println!("Digite seu palpite:");
+
+        io::stdin()
+            .read_line(&mut entrada)
+            .expect("Erro ao ler");
+
+        let palpite: i32 = match entrada.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Por favor, digite um número válido!\n");
+                continue;
+            }
+        };
+
+        if acertou_o_alvo(palpite, numero_secreto) {
+            let distancia = (palpite - numero_secreto).abs();
+            println!("Parabéns, você acertou o alvo!");
+            println!("Você ficou a apenas {} unidade(s) do número secreto ({}).\n", distancia, numero_secreto);
+            break;
+        } else {
+            println!("Você passou longe! Tente novamente.\n");
+        }
+    }
+}
