3490ab658dbfde579b7fe8d8dd63e88137de5f73
Lavnia005 <laviniaandrade005@gmail.com>
2026-09-09 09:17:08 -0300

    Add ticket validation for VIP access
    Implement ticket validation for VIP access based on sector and code.

---
+++ Relatórios/Relatório 04/Exercício 04.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func validarIngresso(setor string, codigo int) bool {
+	if setor == "VIP" && codigo == 2026 {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	for {
+		var setor string
+		var codigo int
+
+		fmt.Print("Digite o setor do ingresso: ")
+		fmt.Scanln(&setor)
+
+		fmt.Print("Digite o código do ingresso: ")
+		fmt.Scanln(&codigo)
+
+		valido := validarIngresso(setor, codigo)
+
+		if valido {
+			fmt.Println("Acesso liberado à área VIP!")
+			break
+		} else {
+			fmt.Println("Ingresso ou setor inválido. Tente novamente.")
+		}
+	}
+}
