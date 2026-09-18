68061e00c91daa6a9d3a46751bcb25b0274cc870
Lavnia005 <laviniaandrade005@gmail.com>
2026-09-17 21:41:08 -0300

    Add reliability calculation program in C++

---
+++ Relatórios/Relatório 05/Exercício 02.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+float calcular_confiabilidade_sistema(float probabilidades[], int tamanho) {
+    float confiabilidade_total = 1.0;
+    for(int i = 0; i < tamanho; i++) {
+        confiabilidade_total *= probabilidades[i];
+    }
+    return confiabilidade_total;
+}
+
+int main() {
+    int n;
+    
+    cout << "Digite a quantidade de componentes do sistema: ";
+    cin >> n;
+    
+    float probabilidades[n]; 
+    
+    for(int i = 0; i < n; i++) {
+        cout << "Digite a probabilidade do componente " << (i + 1) << " (ex: 0.95): ";
+        cin >> probabilidades[i];
+    }
+    
+    float resultado = calcular_confiabilidade_sistema(probabilidades, n);
+   
+    cout << "Confiabilidade total do sistema: " << resultado << " (" << (resultado * 100) << "%)" << endl;
+    
+    return 0;
+}
